ac99fef13194a0b01f1e0223f260e451aa8e9027
Victorio Berra <tory.berra@wustl.edu>
2019-10-07 15:01:22 -0500

    Initial Commit

---
+++ src/Pumpkin.Beer.Taste/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pumpkin.Beer.Taste.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
